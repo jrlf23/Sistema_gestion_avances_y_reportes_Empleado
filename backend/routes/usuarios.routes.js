375dfe9742f77c8aaddf2349860d2823eb58d764
Jaime Lazo <lazoferman.jaime@gmail.com>
2025-06-26 23:11:43 -0600

    Update backend 2

--- backend/routes/usuarios.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { obtenerUsuarios } = require('../controllers/usuarios.controller');
-
-router.get('/', obtenerUsuarios);
-
-module.exports = router;

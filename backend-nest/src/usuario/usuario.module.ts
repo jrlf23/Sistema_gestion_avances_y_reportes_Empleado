4f63fdd22bef3a47ed185f1da26ed100d7ffe903
Jaime Lazo <lazoferman.jaime@gmail.com>
2025-06-27 12:06:38 -0600

    Update 3

--- backend-nest/src/usuario/usuario.module.ts
+++ backend-nest/src/usuario/usuario.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
+import { Usuario } from './usuario.entity';
+import { TypeORMError } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Usuario])],
   providers: [UsuariosService],
-  controllers: [UsuarioController]
+  controllers: [UsuarioController],
+  exports: [UsuariosService]
 })
 export class UsuarioModule {}
